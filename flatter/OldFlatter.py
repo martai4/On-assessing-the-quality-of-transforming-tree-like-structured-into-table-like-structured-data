c86da680ed96cde378f91739a6c9212ed40e9c92
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2024-04-18 14:46:27 +0200

    Old flatter to another file

---
+++ flatter/OldFlatter.py
@@ -0,0 +1,36 @@
+import threading
+
+from methods.JSONFirstListFlattener import JSONFirstListFlattener
+from methods.JSONPathFlattener import JSONPathFlattener
+from methods.JSONListToTableConverter import JSONListToTableConverter
+from methods.FlattenJSON import FlattenJSON
+
+# todo - delete
+if __name__ == "__main__": 
+    json_file_paths = ['../data/airlines.json',
+                       '../data/gists.json',
+                       '../data/movies.json',
+                       '../data/reddit.json',
+                       '../data/nasa.json']
+
+    json_path_flattener = JSONPathFlattener()
+    json_path_flattener_thread = threading.Thread(target=json_path_flattener.serve, args=(json_file_paths, 50051))
+    json_path_flattener_thread.start()
+
+    json_first_list_flattener = JSONFirstListFlattener()
+    json_first_list_flattener_thread = threading.Thread(target=json_first_list_flattener.serve, args=(json_file_paths, 50052))
+    json_first_list_flattener_thread.start()
+
+    json_list_to_table_converter = JSONListToTableConverter()
+    json_list_to_table_converter_thread = threading.Thread(target=json_list_to_table_converter.serve,
+                                                           args=(json_file_paths, 50053))
+    json_list_to_table_converter_thread.start()
+
+    flatten_json = FlattenJSON()
+    flatten_json_thread = threading.Thread(target=flatten_json.serve, args=(json_file_paths, 50054))
+    flatten_json_thread.start()
+
+    json_path_flattener_thread.join()
+    json_first_list_flattener_thread.join()
+    json_list_to_table_converter_thread.join()
+    flatten_json_thread.join()
--- flatter/api.py
+++ flatter/api.py
@@ -1,15 +1,8 @@
-# python -m  uvicorn api:app --reload --port 8000
+# python -m uvicorn api:app --reload --port 8000
 
 import socket, time, asyncio
 from json import loads
 from fastapi import FastAPI
-import threading
-
-from methods.JSONFirstListFlattener import JSONFirstListFlattener
-from methods.JSONPathFlattener import JSONPathFlattener
-from methods.JSONListToTableConverter import JSONListToTableConverter
-from methods.FlattenJSON import FlattenJSON
-
 
 app = FastAPI()
 
@@ -21,13 +14,13 @@ async def root():
 
 @app.post("/socket-test/{port}")
 async def socket_test(port: int) -> str:
-    asyncio.create_task(socket_test_temp(port))
+    asyncio.create_task(socket_test_task(port))
     time.sleep(1)  # Wait for the socket to be set
 
     return "OK"
 
-async def socket_test_temp(port: int):
-    print("test")
+
+async def socket_test_task(port: int):
     host = '127.0.0.1'
     buffer_size = 256 * 1024
 
@@ -60,33 +53,3 @@ async def socket_test_temp(port: int):
         finally:
             conn.close()
             print("Test finished successfully")
-
-@app.post("/thread-test")
-async def tread_test():
-    json_file_paths = ['../data/airlines.json',
-                       '../data/gists.json',
-                       '../data/movies.json',
-                       '../data/reddit.json',
-                       '../data/nasa.json']
-
-
-    json_path_flattener = JSONPathFlattener()
-    json_path_flattener_thread = threading.Thread(target=json_path_flattener.serve, args=(json_file_paths, 50051))
-    json_path_flattener_thread.start()
-
-    json_first_list_flattener = JSONFirstListFlattener()
-    json_first_list_flattener_thread = threading.Thread(target=json_first_list_flattener.serve, args=(json_file_paths, 50052))
-    json_first_list_flattener_thread.start()
-
-    json_list_to_table_converter = JSONListToTableConverter()
-    json_list_to_table_converter_thread = threading.Thread(target=json_list_to_table_converter.serve, args=(json_file_paths, 50053))
-    json_list_to_table_converter_thread.start()
-
-    flatten_json = FlattenJSON()
-    flatten_json_thread = threading.Thread(target=flatten_json.serve, args=(json_file_paths, 50054))
-    flatten_json_thread.start()
-
-    json_path_flattener_thread.join()
-    json_first_list_flattener_thread.join()
-    json_list_to_table_converter_thread.join()
-    flatten_json_thread.join()
