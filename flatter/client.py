4afde6708dcc6a1d31559e77aa76cd9fdfc82c2c
martai4 <tusiulka2011@gmail.com>
2024-05-19 16:13:56 +0200

    change table names

--- flatter/client.py
+++ flatter/client.py
@@ -17,61 +17,61 @@ def client_example():
             # print(data)
 #Movies
 def client_reddit():
-    # port = 50051
-    # client = flight.FlightClient(f"grpc+tcp://localhost:{port}")
+    port = 50051
+    client = flight.FlightClient(f"grpc+tcp://localhost:{port}")
 
-    # table_name = 'JSONPath_movies'
-    # reader = client.do_get(flight.Ticket(table_name.encode()))
-    # data = reader.read_all()
+    table_name = 'JSONPath_movies'
+    reader = client.do_get(flight.Ticket(table_name.encode()))
+    data = reader.read_all()
 
-    # # SELECTION
-    # # first level query
-    # ## to string
-    # print(data.select(['title']))
-    # ## to int
-    # print(data.select(['year']))
-    # ## object from list 
-    # ### low level of nulls - first element of list
-    # print(data.select(['cast[0]']))
-    # ### medium level of nulls
-    # print(data.select(['cast[9]']))
-    # ### high level of nulls - last element of list
-    # print(data.select(['cast[58]']))
+    # SELECTION
+    # first level query
+    ## to string
+    print(data.select(['title']))
+    ## to int
+    print(data.select(['year']))
+    ## object from list 
+    ### low level of nulls - first element of list
+    print(data.select(['cast[0]']))
+    ### medium level of nulls
+    print(data.select(['cast[9]']))
+    ### high level of nulls - last element of list
+    print(data.select(['cast[58]']))
 
-    # # # FILTRES
-    # print(pc.filter(data, pc.greater(data.column('year'), 2000)))
+    # # FILTRES
+    print(pc.filter(data, pc.greater(data.column('year'), 2000)))
 
-    # # #SORT
-    # print(pc.take(data,pc.sort_indices(data, sort_keys=[("title", "ascending")])))
-    # # #SORT DESC
-    # print(pc.take(data,pc.sort_indices(data, sort_keys=[("title", "descending")])))
+    # #SORT
+    print(pc.take(data,pc.sort_indices(data, sort_keys=[("title", "ascending")])))
+    # #SORT DESC
+    print(pc.take(data,pc.sort_indices(data, sort_keys=[("title", "descending")])))
 
-    # #GROUP BY
-    # print(pa.TableGroupBy(data,'year'))
-    # print(pa.TableGroupBy(data,'genres[0]'))
-    # print(pa.TableGroupBy(data, ['genres[0]','genres[1]']))
-    # print(pa.TableGroupBy(data,'cast[0]'))
+    #GROUP BY
+    print(pa.TableGroupBy(data,'year'))
+    print(pa.TableGroupBy(data,'genres[0]'))
+    print(pa.TableGroupBy(data, ['genres[0]','genres[1]']))
+    print(pa.TableGroupBy(data,'cast[0]'))
 
-    # #AGGREGATE FUNCTION
-    # print(pa.TableGroupBy(data,'year').aggregate([("year", "count")]))
-    # print(pa.TableGroupBy(data, 'genres[0]').aggregate([('genres[0]', "count")]))
-    # print(pa.TableGroupBy(data, ['genres[0]','genres[1]']).aggregate([('genres[0]', "count"),('genres[1]', "count")]))
+    #AGGREGATE FUNCTION
+    print(pa.TableGroupBy(data,'year').aggregate([("year", "count")]))
+    print(pa.TableGroupBy(data, 'genres[0]').aggregate([('genres[0]', "count")]))
+    print(pa.TableGroupBy(data, ['genres[0]','genres[1]']).aggregate([('genres[0]', "count"),('genres[1]', "count")]))
 
 
-    # combined_df = []
-    # for genre in filter(lambda x: ('genre' in x), data.schema.names):
-    #     print(genre)
-    #     df_genre = data.select([genre]).rename_columns(['genre'])
-    #     combined_df.append(df_genre)
+    combined_df = []
+    for genre in filter(lambda x: ('genre' in x), data.schema.names):
+        print(genre)
+        df_genre = data.select([genre]).rename_columns(['genre'])
+        combined_df.append(df_genre)
     
-    # combined_df = pa.concat_tables(combined_df)
-    # print(combined_df)
-    # combined_df = combined_df.combine_chunks()
-    # print(pa.TableGroupBy(combined_df, ['genre']).aggregate([('genre', "count")]))
+    combined_df = pa.concat_tables(combined_df)
+    print(combined_df)
+    combined_df = combined_df.combine_chunks()
+    print(pa.TableGroupBy(combined_df, ['genre']).aggregate([('genre', "count")]))
 
     # port = 50054
     # client = flight.FlightClient(f"grpc+tcp://localhost:{port}")
-    # table_name = 'FlattenedJSON_movies'
+    # table_name = 'SimpleMethod_movies'
     # reader = client.do_get(flight.Ticket(table_name.encode()))
     # data = reader.read_all()
     
@@ -123,16 +123,16 @@ def client_reddit():
     # reader = client.do_get(flight.Ticket(table_name.encode()))
     # data = reader.read_all()
 
-    port = 50052
-    client = flight.FlightClient(f"grpc+tcp://localhost:{port}")
-    table_name = 'FlattenedFirstJSON_movies'
-    reader = client.do_get(flight.Ticket(table_name.encode()))
-    data = reader.read_all()
-    print(data)
+    # port = 50052
+    # client = flight.FlightClient(f"grpc+tcp://localhost:{port}")
+    # table_name = 'FlattenedFirstJSON_movies'
+    # reader = client.do_get(flight.Ticket(table_name.encode()))
+    # data = reader.read_all()
+    # print(data)
 
 if __name__ == '__main__':
-    client_reddit()
-    # client_example()
+    # client_reddit()
+    client_example()
 # title: string
 # year: int64
 # cast: list<item: string>
--- flatter/methods/JSONPathFlattener.py
+++ flatter/methods/JSONPathFlattener.py
@@ -15,14 +15,14 @@ class JSONPathFlattener:
         self.server.serve()
 
     def do_put(self, dataset_name: str, json) -> None:
-        flat_data = {f"FlattenedJSON_{dataset_name}": self.flatten_json(json)}
+        flat_data = {f"JSONPath_{dataset_name}": self.flatten_json(json)}
         self.server.do_put(flat_data)
 
     def load_json_from_file(self, file_paths: list) -> None:
         flattened_data = {}
         for path in file_paths:
             json_data = json.load(open(path, encoding='utf-8'))
-            flattened_data[f"FlattenedJSON_{os.path.basename(path)}"] = self.flatten_json(json_data)
+            flattened_data[f"JSONPath_{os.path.basename(path)}"] = self.flatten_json(json_data)
 
         self.server.do_put(flattened_data)
 
