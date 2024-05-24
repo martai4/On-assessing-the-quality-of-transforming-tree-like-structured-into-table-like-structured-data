70c4fec1fbf6e8d42bd16e6e707cdeaaa8c9ce9e
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2024-05-24 12:52:34 +0200

    disable add_middleware

--- flatter/api.py
+++ flatter/api.py
@@ -11,7 +11,7 @@ import Utils
 from Statisticker import Statisticker
 
 app = FastAPI()
-app.add_middleware(PyInstrumentProfilerMiddleware)
+# app.add_middleware(PyInstrumentProfilerMiddleware)
 statisticker = Statisticker()
 
 
