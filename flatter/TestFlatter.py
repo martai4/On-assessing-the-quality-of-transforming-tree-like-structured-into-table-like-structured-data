7b26090915ac82e49dfa9eb3ddcde2f1c5fda0e8
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2024-07-28 16:46:41 +0200

    wykresy

--- flatter/TestFlatter.py
+++ flatter/TestFlatter.py
@@ -1,4 +1,5 @@
 import threading
+import itertools
 
 from Statisticker import Statisticker
 from methods.JSONFirstListFlattener import JSONFirstListFlattener
@@ -8,23 +9,15 @@ from methods.JSONFlatten import JSONFlatten
 from methods.JSONDummy import JSONDummy
 
 if __name__ == "__main__":
-    TEST_LOOPS = 1
+    TEST_LOOPS = 12
 
-    knowledge_files = ['../data/airlines.json',
-                       '../data/gists.json',
-                       '../data/movies.json',
-                       '../data/reddit.json',
-                       '../data/nasa.json']
-
-    test_name = "AIRLINES-300000"
-    # GISTS-100000
-    # AIRLINES-300000
-    # MOVIES-500000
-    # NASA-1000000
-    # REDDIT-5000
-
-    file_name = "../data/testFiles/" + test_name
-    files_to_check = 1
+    files_to_check = [
+        "airlines",
+        "gists",
+        "movies",
+        "reddit",
+        "nasa",
+    ]
 
     statisticker = Statisticker()
     # monitor_thread = threading.Thread(target=statisticker.start_monitoring, args=("statistics",))
@@ -35,20 +28,21 @@ if __name__ == "__main__":
         (JSONFirstListFlattener(), 50052, "JSONFirstListFlattener"),
         (JSONListToTableConverter(), 50053, "JSONListToTableConverter"),
         (JSONFlatten(), 50054, "JSONFlatten"),
-        (JSONDummy(), 50055, "JSONDummy")
+        (JSONDummy(), 50055, "JSONDummy"),
     ]
 
-    for i in range(TEST_LOOPS):
-        print(f"Loop nr: {i}")
-        for (flatter, port, method_name) in flatter_list:
+    for loop, test_file in itertools.product(range(TEST_LOOPS), files_to_check):
+
+        print(f"Loop nr: {loop}")
+        for flatter, port, method_name in flatter_list:
             thread = threading.Thread(target=flatter.serve, args=(port,))
             thread.start()
 
             print(f"--- {method_name} ---")
             statisticker.start_measuring_time()
             # flatter.load_json_from_file(knowledge_files) # Knowledge files
-            flatter.load_json_from_file([f"{file_name}-{i}.txt" for i in range(files_to_check)])
-            statisticker.stop_measuring_time(f"tests/time/files/{test_name}-{method_name}")
+            flatter.load_json_from_file([f"../data/testFiles/{test_file}.txt"])
+            statisticker.stop_measuring_time(f"tests/time/{test_file}-{method_name}")
 
             flatter.server.stop()
             thread.join()
